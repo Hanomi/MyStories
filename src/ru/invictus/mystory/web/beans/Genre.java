caab07ab7e4d20bdd9b8d58fd2842e106da8f23a
Hanomi <freefpdie@gmail.com>
2018-05-07 13:28:49 +0700

    GenreList

---
+++ src/ru/invictus/mystory/web/beans/Genre.java
@@ -0,0 +1,20 @@
+package ru.invictus.mystory.web.beans;
+
+public class Genre {
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/ru/invictus/mystory/web/beans/GenreList.java
@@ -0,0 +1,38 @@
+package ru.invictus.mystory.web.beans;
+
+import ru.invictus.mystory.web.db.Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GenreList {
+    private static List<Genre> genreList = new ArrayList<>();
+
+    private GenreList() {}
+
+    private static void getGenres() {
+        try (Connection connection = Database.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * from mystory.genre order by name")) {
+            while (resultSet.next()) {
+                Genre genre = new Genre(resultSet.getString("name"));
+                genreList.add(genre);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(AuthorList.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public static List<Genre> getBookList() {
+        if (genreList.isEmpty()) {
+            getGenres();
+        }
+        return genreList;
+    }
+}
