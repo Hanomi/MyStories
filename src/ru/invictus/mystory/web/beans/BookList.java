13dbb5b1d35e63093f71a76911b7a388c22dd4b3
Hanomi <freefpdie@gmail.com>
2018-05-07 13:25:16 +0700

    BookList

--- src/ru/invictus/mystory/web/beans/BookList.java
+++ src/ru/invictus/mystory/web/beans/BookList.java
@@ -11,29 +11,29 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class AuthorList {
-    private static List<Author> authorList = new ArrayList<>();
+public class BookList {
+    private static List<Book> bookList = new ArrayList<>();
 
-    private AuthorList() {}
+    private BookList() {}
 
-    private static void getAuthors() {
+    private static void getBooks() {
         try (Connection connection = Database.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * from mystory.author order by fio")) {
+             ResultSet resultSet = statement.executeQuery("SELECT * from mystory.book order by name")) {
             while (resultSet.next()) {
-                Author author = new Author();
-                author.setName(resultSet.getString("fio"));
-                authorList.add(author);
+                Book book = new Book();
+                book.setName(resultSet.getString("name"));
+                bookList.add(book);
             }
         } catch (SQLException e) {
             Logger.getLogger(AuthorList.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
-    public static List<Author> getAuthorList() {
-        if (authorList.isEmpty()) {
-            getAuthors();
+    public static List<Book> getBookList() {
+        if (bookList.isEmpty()) {
+            getBooks();
         }
-        return authorList;
+        return bookList;
     }
 }
