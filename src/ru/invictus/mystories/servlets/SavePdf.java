75a4b5b65a3cbb82b715e5671554e810687173b5
Hanomi <freefpdie@gmail.com>
2018-05-12 18:15:40 +0700

    read/save pdf

--- src/ru/invictus/mystories/servlets/SavePdf.java
+++ src/ru/invictus/mystories/servlets/SavePdf.java
@@ -4,14 +4,16 @@ import ru.invictus.mystories.controller.SearchController;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URLEncoder;
 
-public class ShowPdf extends HttpServlet {
-
+@WebServlet(name = "SavePdf", urlPatterns = {"/download", "/read"})
+public class SavePdf extends HttpServlet {
     @Inject
     private SearchController searchController;
 
@@ -27,6 +29,10 @@ public class ShowPdf extends HttpServlet {
             String id = req.getParameter("id");
             byte[] pdf = searchController.getData(id, FileType.PDF);
             resp.setContentLength(pdf.length);
+            if ("/download".equals(req.getServletPath())) {
+                String filename = URLEncoder.encode(req.getParameter("filename"), "UTF-8").replace("+", " ");
+                resp.setHeader("Content-Disposition", "attachment;filename=" + filename + ".pdf");
+            }
             out.write(pdf);
         }
     }
