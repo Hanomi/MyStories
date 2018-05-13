90e939a5cf54d2775f5fd448038f1577257fe535
Hanomi <freefpdie@gmail.com>
2018-05-13 16:51:15 +0700

    showimage servlet annotation add secure option

--- src/ru/invictus/mystories/servlets/ShowImage.java
+++ src/ru/invictus/mystories/servlets/ShowImage.java
@@ -4,12 +4,14 @@ import ru.invictus.mystories.controller.SearchController;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
+@WebServlet(name = "ShowImage", urlPatterns = {"/image"})
 public class ShowImage extends HttpServlet {
 
     @Inject
